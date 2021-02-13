70b74f6a732928c16a26d3037d8abb8fa5178d5a
yinweijie <ywj123450@gmail.com>
2021-02-14 00:40:08 +0800

    0103 completed

--- array-and-string/0103-Solution.cpp
+++ array-and-string/0103-Solution.cpp
@@ -2,12 +2,25 @@
 // https://leetcode-cn.com/leetbook/read/array-and-string/yf47s/
 
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
+        int sumAll = accumulate(nums.begin(), nums.end(), 0);
 
+        int sumPre = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            // sumPre == sumAll - sumPre - nums[i]
+            if(sumPre * 2 == sumAll - nums[i]) {
+                return i;
+            }
+
+            sumPre += nums[i];
+        }
+
+        return -1;
     }
 };
