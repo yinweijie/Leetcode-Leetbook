0a2fa06cd84b6f00356ed78f92328cf09dbc22d2
yinweijie <ywj123450@gmail.com>
2021-03-01 21:33:07 +0800

    0204 completed

---
+++ data-structure-binary-tree/0204-Solution.cpp
@@ -0,0 +1,28 @@
+// 路径总和
+// https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xo566j/
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr) return false;
+
+        if(root->left == nullptr && root->right == nullptr) {
+            return root->val == targetSum;
+        }
+
+        return hasPathSum(root->left, targetSum - root->val) ||
+                hasPathSum(root->right, targetSum - root->val);
+    }
+};
