dc6f94f0da8354ac0dd397e0acf0c49bf0cf5070
yinweijie <ywj123450@gmail.com>
2021-03-06 16:15:51 +0800

    0301 completed

--- data-structure-binary-tree/0301-Solution.cpp
+++ data-structure-binary-tree/0301-Solution.cpp
@@ -2,6 +2,7 @@
 // https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xo98qt/
 
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -17,9 +18,31 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// 参考：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/solution/tu-jie-gou-zao-er-cha-shu-wei-wan-dai-xu-by-user72/
 class Solution {
+private:
+    unordered_map<int, int> mp; // <number, index>
+    vector<int> _postorder;
+
+    TreeNode* buildTree(int iStart, int iEnd, int pStart, int pEnd) {
+        if(iStart > iEnd || pStart > pEnd) return nullptr;
+
+        auto[iNumber, iIndex] = *mp.find(_postorder[pEnd]);
+        TreeNode* node = new TreeNode(iNumber);
+        node->left = buildTree(iStart, iIndex - 1, pStart, pStart + iIndex - iStart - 1);
+        node->right = buildTree(iIndex + 1, iEnd, pStart + iIndex - iStart, pEnd - 1);
+
+        return node;
+    }
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        for(int i = 0; i < inorder.size(); i++) {
+            mp[inorder[i]] = i;
+        }
 
+        _postorder = postorder;
+
+        TreeNode* res = buildTree(0, inorder.size() - 1, 0, postorder.size() - 1);
+        return res;
     }
 };
