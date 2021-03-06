ba9bf176b0669e6b1aaac0b76c0ebda171637347
Your Name <you@example.com>
2021-03-07 00:49:34 +0800

    0306 updated

---
+++ data-structure-binary-tree/0306-Solution.cpp
@@ -0,0 +1,34 @@
+// 二叉树的序列化与反序列化
+// https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xomr73/
+
+#include <string>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
