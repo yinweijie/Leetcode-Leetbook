45e84bf9734e6854c6c0a01ad84d6e7f45f13004
yinweijie <ywj123450@gmail.com>
2021-03-07 20:49:15 +0800

    0306 completed

--- data-structure-binary-tree/0306-Solution.cpp
+++ data-structure-binary-tree/0306-Solution.cpp
@@ -2,6 +2,8 @@
 // https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xomr73/
 
 #include <string>
+#include <cstring>
+#include <queue>
 
 using namespace std;
 
@@ -16,16 +18,50 @@ struct TreeNode {
 };
 
 class Codec {
+private:
+    void serialize(TreeNode* node, string& str) {
+        if(node == nullptr) {
+            str += "NULL,";
+            return;
+        }
+
+        str += to_string(node->val) + ",";
+        serialize(node->left, str);
+        serialize(node->right, str);
+    }
+
+    TreeNode* deserialize(queue<string>& q) {
+        if(q.front() == "NULL") {
+            q.pop();
+            return nullptr;
+        }
+
+        TreeNode* node = new TreeNode(stoi(q.front()));
+        q.pop();
+        node->left = deserialize(q);
+        node->right = deserialize(q);
+
+        return node;
+    }
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        
+        string str = "";
+        serialize(root, str);
+        return str;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        
+        queue<string> q;
+        char* token = strtok(data.data(), ",");
+        while(token) {
+            q.emplace(string(token));
+            token = strtok(NULL, ",");
+        }
+
+        return deserialize(q);
     }
 };
 
