6d304ed63da1f4b2d152f70e8d262e4df70fdab4
yinweijie <ywj123450@gmail.com>
2021-03-15 14:16:03 +0800

    0502 completed

--- hash-table/0502-Solution.cpp
+++ hash-table/0502-Solution.cpp
@@ -4,12 +4,26 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> hashMap;
 
+        for(string& str : strs) {
+            string strSorted = str;
+            sort(strSorted.begin(), strSorted.end());
+            hashMap[strSorted].emplace_back(str);
+        }
+
+        for(auto it = hashMap.begin(); it != hashMap.end(); it++) {
+            res.emplace_back(it->second);
+        }
+
+        return res;
     }
 };
