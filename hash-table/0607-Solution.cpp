f4e3401067ea374adb71166f6979fa70108fe3e4
yinweijie <ywj123450@gmail.com>
2021-03-16 22:57:58 +0800

    0607 updated

---
+++ hash-table/0607-Solution.cpp
@@ -0,0 +1,34 @@
+// 常数时间插入、删除和获取随机元素
+// https://leetcode-cn.com/leetbook/read/hash-table/xx0zpt/
+
+class RandomizedSet {
+    
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
