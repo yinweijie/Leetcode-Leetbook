cc90a1eab2f75152c2bc2d4ea5451d422523d495
yinweijie <ywj123450@gmail.com>
2021-03-16 11:35:36 +0800

    0604 completed

--- hash-table/0604-Solution.cpp
+++ hash-table/0604-Solution.cpp
@@ -2,27 +2,26 @@
 // https://leetcode-cn.com/leetbook/read/hash-table/xxwhng/
 
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-        unordered_set<int> sumAB;
-        unordered_set<int> sumCD;
+        unordered_map<int, int> sumAB;
         int res = 0;
 
         for(int i = 0; i < A.size(); i++) {
             for(int j = 0; j < B.size(); j++) {
-                sumAB.insert(A[i] + B[j]);
+                sumAB[A[i] + B[j]]++;
             }
         }
 
         for(int i = 0; i < C.size(); i++) {
             for(int j = 0; j < D.size(); j++) {
                 if(sumAB.find(-(C[i] + D[j])) != sumAB.end()) {
-                    res++;
+                    res += sumAB[-(C[i] + D[j])];
                 }
             }
         }
