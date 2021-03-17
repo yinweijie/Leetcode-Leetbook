d61b6cab3a724f25b4f92cd79a663328f592d97c
yinweijie <ywj123450@gmail.com>
2021-03-17 20:34:20 +0800

    0607 completed

--- hash-table/0607-Solution.cpp
+++ hash-table/0607-Solution.cpp
@@ -1,7 +1,17 @@
 // 常数时间插入、删除和获取随机元素
 // https://leetcode-cn.com/leetbook/read/hash-table/xx0zpt/
 
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
 class RandomizedSet {
+private:
+    vector<int> values;
+    // <number, index>
+    unordered_map<int, int> hashMap;
     
 public:
     /** Initialize your data structure here. */
@@ -11,17 +21,33 @@ public:
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
+        if(hashMap.find(val) != hashMap.end()) return false;
 
+        values.emplace_back(val);
+        hashMap[val] = values.size() - 1;
+
+        return true;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
+        if(hashMap.find(val) == hashMap.end()) return false;
 
+        int idx = hashMap[val];
+
+        swap(values[idx], values[values.size() - 1]);
+        values.pop_back();
+
+        hashMap[values[idx]] = idx;
+        hashMap.erase(val);
+
+        return true;
     }
     
     /** Get a random element from the set. */
     int getRandom() {
-
+        int randIndex = rand() % (values.size());
+        return values[randIndex];
     }
 };
 
