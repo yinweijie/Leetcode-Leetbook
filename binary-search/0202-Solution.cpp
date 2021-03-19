5fe8df0d6ce9065f75d8bd3ec37909bf83b23e6e
yinweijie <ywj123450@gmail.com>
2021-03-19 20:38:52 +0800

    0202 completed

---
+++ binary-search/0202-Solution.cpp
@@ -0,0 +1,27 @@
+// 二分查找
+// https://leetcode-cn.com/leetbook/read/binary-search/xexoac/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
