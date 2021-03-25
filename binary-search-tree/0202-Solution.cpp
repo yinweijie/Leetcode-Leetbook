095e93b3bdc9caed3f4e086f2545d49d7052264a
yinweijie <ywj123450@gmail.com>
2021-03-25 22:56:45 +0800

    0202 updated

---
+++ binary-search-tree/0202-Solution.cpp
@@ -0,0 +1,20 @@
+// 验证二叉搜索树
+// https://leetcode-cn.com/leetbook/read/introduction-to-data-structure-binary-search-tree/xpkc6i/
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+    }
+};
