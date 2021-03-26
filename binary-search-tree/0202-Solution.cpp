0b84590d65b119e2b7e8b381354def905bf4a187
yinweijie <ywj123450@gmail.com>
2021-03-26 21:45:26 +0800

    0202 completed

--- binary-search-tree/0202-Solution.cpp
+++ binary-search-tree/0202-Solution.cpp
@@ -1,6 +1,10 @@
 // 验证二叉搜索树
 // https://leetcode-cn.com/leetbook/read/introduction-to-data-structure-binary-search-tree/xpkc6i/
 
+#include <limits>
+
+using namespace std;
+
 /**
  * Definition for a binary tree node.
  */
@@ -14,7 +18,29 @@ struct TreeNode {
 };
 
 class Solution {
+private:
+    bool isBounded(TreeNode* node, long long upper, long long lower) {
+        if(node == nullptr) return true;
+
+        if(node->val >= upper || node->val <= lower) {
+            return false;
+        }
+
+        return isBounded(node->left, node->val, lower) &&
+                isBounded(node->right, upper, node->val);
+    }
+
 public:
     bool isValidBST(TreeNode* root) {
+        return isBounded(root, numeric_limits<long long>::max(), numeric_limits<long long>::min());
     }
 };
+
+int main() {
+    TreeNode* root = new TreeNode(2);
+    root->left = new TreeNode(1);
+    root->right = new TreeNode(3);
+
+    Solution().isValidBST(root);
+    return 0;
+}
