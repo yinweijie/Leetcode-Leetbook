c3bb51c29413b364490aa97f8fccb43107f45f3b
yinweijie <ywj123450@gmail.com>
2021-03-26 22:25:35 +0800

    0302 completed

---
+++ binary-search-tree/0302-Solution.cpp
@@ -0,0 +1,29 @@
+// 二叉搜索树中的搜索
+// https://leetcode-cn.com/leetbook/read/introduction-to-data-structure-binary-search-tree/xpsqtv/
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == nullptr) return nullptr;
+
+        if(root->val == val) return root;
+
+        if(val > root->val) {
+            return searchBST(root->right, val);
+        } else {
+            return searchBST(root->left, val);
+        }
+    }
+};
