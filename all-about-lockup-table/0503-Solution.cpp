cde61238603a4792ca72f865d3a7bd24e21e5842
yinweijie <ywj123450@gmail.com>
2021-04-04 21:59:26 +0800

    0503 completed

---
+++ all-about-lockup-table/0503-Solution.cpp
@@ -0,0 +1,28 @@
+// 字母异位词分组
+// https://leetcode-cn.com/leetbook/read/all-about-lockup-table/xhofoh/
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hashMap;
+        for(const string& str : strs) {
+            string strSorted = str;
+            sort(strSorted.begin(), strSorted.end());
+            hashMap[strSorted].emplace_back(str);
+        }
+
+        vector<vector<string>> res;
+        for(auto&[strSorted, vecStrs] : hashMap) {
+            res.emplace_back(vecStrs);
+        }
+
+        return res;
+    }
+};
