09b9c31ebbfa56567056ba61fa185cd3c8cd12d3
yinweijie <ywj123450@gmail.com>
2021-04-04 21:22:58 +0800

    0405 completed

--- all-about-lockup-table/0405-Solution.cpp
+++ all-about-lockup-table/0405-Solution.cpp
@@ -9,15 +9,56 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-
-        if(nums[0] > target) return vector<vector<int>>();
         if(nums.size() < 4) return vector<vector<int>>();
 
-        for(int k1 = 0; k1 <= nums.size() - 4; k1++) {
-            for(int k2 = k1 + 1; k2 <= nums.size() - 3; k2++) {
+        sort(nums.begin(), nums.end());
 
+        // 这里不需要这个判断条件，序列可能有小于0的数，nums序列的和有可能是先递减再递增
+        // 但是对于三数之和这道题，如果序列第一个数大于0，nums序列的和就是递增的
+        // if(nums[0] > target) return vector<vector<int>>();
+
+        vector<vector<int>> res;
+        for(int k1 = 0; k1 <= nums.size() - 4; k1++) {
+            if(k1 > 0 && nums[k1] == nums[k1 - 1]) continue;
+
+            for(int k2 = k1 + 1; k2 <= nums.size() - 3; k2++) {
+                // k2的有效位置判断也需要注意，每次循环的第一个数都是有效的，第一个数以后才需要特判
+                if(k2 > k1 + 1 && nums[k2] == nums[k2 - 1]) continue;
+
+                int i = k2 + 1, j = nums.size() - 1;
+
+                while(i < j) {
+                    int numSum = nums[k1] + nums[k2] + nums[i] + nums[j];
+                    if(numSum == target) {
+                        res.emplace_back(vector<int> {nums[k1], nums[k2], nums[i], nums[j]});
+
+                        i++;
+                        while(i < j && nums[i] == nums[i - 1]) {
+                            i++;
+                        }
+                        if(i == j) {
+                            break;
+                        }
+
+                        j--;
+                        while(i < j && nums[j] == nums[j + 1]) {
+                            j--;
+                        }
+                    } else if(numSum > target) {
+                        j--;
+                    } else {
+                        i++;
+                    }
+                }
             }
         }
+
+        return res;
     }
 };
+
+int main() {
+    vector<int> nums {1,-2,-5,-4,-3,3,3,5};
+    Solution().fourSum(nums, -11);
+    return 0;
+}
