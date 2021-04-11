6687cf388994f6dedfa8460873f368ecd900a27b
yinweijie <ywj123450@gmail.com>
2021-04-12 01:21:29 +0800

    0408 updated

---
+++ hash-table-plus/0408-Solution.cpp
@@ -0,0 +1,14 @@
+// 替换后的最长重复字符
+// https://leetcode-cn.com/leetbook/read/hash-table-plus/x7auk1/
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+    }
+};
