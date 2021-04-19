1e1f0782f8cf6ebdc63576c4ce0100c1013c1ae4
yinweijie <ywj123450@gmail.com>
2021-04-19 22:41:30 +0800

    0204 updated

---
+++ sort-algorithms/0204-Solution.cpp
@@ -0,0 +1,13 @@
+// 283. 移动零
+// https://leetcode-cn.com/leetbook/read/sort-algorithms/evfj1d/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+    }
+};
