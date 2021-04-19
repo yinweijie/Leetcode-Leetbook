bdeb39cd3b0602659cf5b519ba2a5d6cfc732517
yinweijie <ywj123450@gmail.com>
2021-04-20 00:01:29 +0800

    0204 completed

--- sort-algorithms/0204-Solution.cpp
+++ sort-algorithms/0204-Solution.cpp
@@ -2,12 +2,22 @@
 // https://leetcode-cn.com/leetbook/read/sort-algorithms/evfj1d/
 
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = n - 1; i >= 0; i--) {
+            if(nums[i] != 0) continue;
 
+            for(int j = i; j + 1 < n; j++) {
+                if(nums[j + 1] != 0) {
+                    swap(nums[j], nums[j + 1]);
+                }
+            }
+        }
     }
 };
