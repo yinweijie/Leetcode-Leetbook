9fea627ca0297bbb4ef540277022f13a0990ae9e
yinweijie <ywj123450@gmail.com>
2021-05-18 11:17:02 +0800

    leetcode1 completed

--- problemset/leetcode1.cpp
+++ problemset/leetcode1.cpp
@@ -2,12 +2,24 @@
 // https://leetcode-cn.com/problems/two-sum/
 
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashMap;
 
+        for(int i = 0; i < nums.size(); i++) {
+            int rest = target - nums[i];
+            if(hashMap.count(rest) > 0) {
+                return vector<int> {hashMap[rest], i};
+            }
+
+            hashMap[nums[i]] = i;
+        }
+
+        return vector<int>();
     }
 };
