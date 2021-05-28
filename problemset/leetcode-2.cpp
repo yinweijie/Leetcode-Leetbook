a18c69f7be0184aa6d9087f32ffc43b9f6289246
yinweijie <ywj123450@gmail.com>
2021-05-28 22:42:57 +0800

    2 completed

--- problemset/leetcode-2.cpp
+++ problemset/leetcode-2.cpp
@@ -15,6 +15,39 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(-1);
+        ListNode* tail = dummyHead;
 
+        int flag = 0;
+        while(l1 || l2) {
+            int l1Num = (l1 != nullptr) ? l1->val : 0;
+            int l2Num = (l2 != nullptr) ? l2->val : 0;
+
+            int sum = l1Num + l2Num + flag;
+
+            int digit = 0;
+            if(sum >= 10) {
+                digit = sum - 10;
+                flag = 1;
+            } else {
+                digit = sum;
+                flag = 0;
+            }
+
+            tail->next = new ListNode(digit);
+            tail = tail->next;
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        }
+        
+        if(flag == 1) {
+            tail->next = new ListNode(1);
+            tail = tail->next;
+        }
+
+        ListNode* res = dummyHead->next;
+        delete dummyHead;
+
+        return res;
     }
 };
